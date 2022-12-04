673cd6cace2ff3351dee8a5865323a752b962904
nemolino <minoli.alessandro@gmail.com>
2022-12-04 15:13:34 +0100

    added feasibility check

--- bpp_instance.py
+++ bpp_instance.py
@@ -1,7 +1,8 @@
 class BPP_Instance:
     
     def __init__(self, e_n, e_vol, c):
-        # number of objects --> 0...len(self.e_vol)-1 (? useful ?)
+        # number of objects 
+        # 0...len(self.e_vol)-1 (? useful ?)
         self.e_n = e_n                        
         # volume of each object
         self.e_vol = e_vol   
@@ -10,9 +11,9 @@ class BPP_Instance:
 
     # returns True if the instance is unfeasible, False otherwise
     def unfeasible(self):
-        return False
+        return max(self.e_vol) > self.c
 
-    # returns some trivial objective bounds [lower_bound, upper_bound] as a tuple
+    # returns some objective bounds [lower_bound, upper_bound] as a tuple
     def obj_bounds(self):
-        import math
-        return (math.ceil(sum(self.e_vol) / self.c), self.e_n)
+        from math import ceil
+        return (ceil(sum(self.e_vol) / self.c), self.e_n)
--- bpp_solution.py
+++ bpp_solution.py
@@ -1,33 +1,50 @@
 class BPP_Solution:
 
     # builds an "empty" solution (unfeasible)
-    def __init__(self, instance):
+    def __init__(self, bpp_instance):
         # BPP_Instance we are working on
-        self.P = instance
+        self.p = bpp_instance
         # e_pos[i] = container in which we place object i
-        self.e_pos = [-1] * self.P.e_n
+        self.e_pos = [-1] * self.p.e_n
         # c_residual[i] = residual capacity of container i
-        self.c_residual = [self.P.c]
+        self.c_residual = []
 
-    # returns True if self representation is consistent, False otherwise : O(?)
-    def consistent(self):
-        return True
-
-    # returns True if self is feasible, False otherwise : O(E)
+    # returns True if self is feasible, False otherwise 
+    # throws an error if self is inconsistent
     def feasible(self):
-        assert self.consistent()
-        return True
 
-    # returns the objective value of self : O(1)
+        is_feasible = True
+        c_residual_check = [self.p.c] * len(self.c_residual)
+
+        for i, pos in enumerate(self.e_pos):
+
+            if pos == -1: 
+                # object still not placed in a container
+                is_feasible = False
+            else: 
+                # False if the object has an invalid position
+                assert 0 <= pos < len(self.c_residual) 
+                c_residual_check[pos] -= self.p.e_vol[i]
+
+        # False if object positions and residual capacities are inconsistent
+        assert self.c_residual == c_residual_check
+
+        # False if we are using an empty container 
+        # ??? not sure if I should check this condition ???
+        assert all(x != self.p.c for x in self.c_residual)
+        
+        return is_feasible
+
+    # returns the objective value of self
+    # throws an error if self is unfeasible or inconsistent
     def obj(self):
-        assert self.consistent()
         assert self.feasible()
         return len(self.c_residual)
 
     # builds a solution using First Fit constructive heuristic
-    def first_fit():
+    def greedy_ff():
         pass
 
-    # builds a solution using Decreasing First Fit constructive heuristic
-    def decreasing_first_fit():
+    # builds a solution using First Fit Decreasing constructive heuristic
+    def greedy_ffd():
         pass
